779182564f360e755095dcd25e5ebd0ff4be0ffe
moltyhero <matanoded@gmail.com>
2022-04-14 02:26:43 +0300

    Finish 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    // let newText = '';
+    // while(text)
+    // {
+    //     newText += text.charAt(text.length - 1);
+    //     if (text.length > 1)
+    //         text = text.substring(0, text.length - 1);
+    //     else text = 0;
+    // }
+    // return newText;
+    return text.split('').reverse().join('');
 };
 
 // Do not edit below this line
