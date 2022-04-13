34cd7c1ff05104700a5d74d234b73c492a13f4c9
moltyhero <matanoded@gmail.com>
2022-04-14 01:49:41 +0300

    Finish first and second excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let finalText = ''
+    for (let i = 0; i < times; i++) {
+        finalText += text;
+    }
+    if (times<0) return "ERROR"
+    return finalText;
 };
 
 // Do not edit below this line
