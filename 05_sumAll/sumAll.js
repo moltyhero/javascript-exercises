07ae028bd717a21dc1802652cba1f74229ef104c
moltyhero <matanoded@gmail.com>
2022-04-14 04:04:06 +0300

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
+	if (
+		typeof firstInt != "number" ||
+		typeof secondInt != "number" || // Non-number parameter
+		firstInt < 0 ||
+		secondInt < 0 // Negative number
+	) {
+		return "ERROR";
+	}
 
+	sum = 0;
+	min = Math.min(firstInt, secondInt);
+	max = Math.max(firstInt, secondInt);
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
