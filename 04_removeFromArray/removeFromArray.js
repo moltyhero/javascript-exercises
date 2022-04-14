ea9ca5fb35d3cf9d04d21ced76058bce02e01be1
moltyhero <matanoded@gmail.com>
2022-04-14 03:54:16 +0300

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,33 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+	/* My Solution:
+    for (let item = 0; item < arr.length; item++) {
+        for (let index = 0; index < args.length; index++) {
+            if (arr[item] === args[index])
+            {
+                arr.splice(item, 1);
+                index = -1;
+            }
+                
+        }
+    }
+    return arr;
+    */
 
+	// Better Solution:
+	const array = arr;
+	// create a new empty array
+	const newArray = [];
+	// use forEach to go through the array
+	array.forEach((item) => {
+		// push every element into the new array
+		// UNLESS it is included in the function arguments
+		// so we create a new array with every item, except those that should be removed
+		if (!args.includes(item)) {
+			newArray.push(item);
+		}
+	});
+	// and return that array
+	return newArray;
 };
 
 // Do not edit below this line
