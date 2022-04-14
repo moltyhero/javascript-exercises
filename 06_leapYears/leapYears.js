1bf9f6b985f42dce36fdb8cc5e2fe96d205eccbc
moltyhero <matanoded@gmail.com>
2022-04-14 04:17:19 +0300

    Finish leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) {
+        return "ERROR";
+    }
+    
+    if  (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        return true;
+    else return false;
 };
 
 // Do not edit below this line
