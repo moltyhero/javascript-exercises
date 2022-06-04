db5844a21c4ab81f0e37bde58d1befb87e53c556
moltyhero <matanoded@gmail.com>
2022-06-04 03:55:29 +0300

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS";
+    let first = 1;
+    let second = 1;
+    let temp;
+    for (let i = 3; i <= num; i++) {
+        temp = second;
+        second += first;
+        first = temp;
+    }
+    return second;
 };
 
 // Do not edit below this line
