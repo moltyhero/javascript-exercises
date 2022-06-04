79c874b0ecf2364016ad47b148c55813b514bb2d
moltyhero <matanoded@gmail.com>
2022-06-04 03:39:36 +0300

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+    let temp = sentence.toLowerCase();
+    let regex = /\W+/g;
+    temp = temp.replace(regex, '');
+    temp = temp.split("");
+    for (let i = 0, j = temp.length-1; i < j; i++, j--) {
+        if (temp[i] != temp[j]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
