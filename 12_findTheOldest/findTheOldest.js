0d36ac4d4602ba89a05031f3469460801820bf2e
moltyhero <matanoded@gmail.com>
2022-06-07 04:45:41 +0300

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sorted = people.sort(function(a,b) {
+        let aAge = 0;
+        let bAge = 0;
+        if (!a.hasOwnProperty('yearOfDeath'))
+        {
+            let today = new Date();
+            aAge = today.getFullYear() - a.yearOfBirth;
+        }
+        else
+        {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.hasOwnProperty('yearOfDeath'))
+        {
+            let today = new Date();
+            bAge = today.getFullYear() - b.yearOfBirth;
+        }
+        else
+        {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        if (aAge < bAge)
+        {
+            return 1;
+        }
+        else
+            return -1;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
